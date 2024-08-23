3a2a916db2cd6a33a0adedcd2962cdea6c38bb85
tim <tim@theconvexlens.co>
2024-08-23 15:40:49 +0100

    firestore updates

--- initialize/firestore.ts
+++ initialize/firestore.ts
@@ -1,23 +1,33 @@
 import {Filter, Firestore} from "@google-cloud/firestore";
 import {fromPromise} from "rxjs/internal/observable/innerFrom";
-import {User} from "../auth/passport/auth.model";
+import {Company, User} from "../auth/passport/auth.model";
+import {map, Observable} from "rxjs";
 
 
 export const firestoreDB = new Firestore({databaseId: 'static'});
 
-export function getUserByEmail(email: string) {
-    return fromPromise(firestoreDB.collection("user").where(Filter.where('Email', '==', email)).get())
+export function getUserByEmail(email: string): Observable<User | undefined> {
+    return fromPromise(firestoreDB.collection("user").where(Filter.where('email', '==', email)).get()).pipe(
+        map(qs => {
+            if (qs.docs.length==0)
+                return undefined;
+            return qs.docs[0].data() as User
+        })
+    )
 }
 
 export function getUserViews(user: User) {
     return fromPromise(
-        firestoreDB.collection("view").where(Filter.where('CompanyUID', '==', user.CompanyUID)).get()).pipe(
+        firestoreDB.collection("view").where(Filter.where('companyUID', '==', user.companyUID)).get()).pipe(
         (dd) => {
             return dd
         }
     )
 }
 
-export function getCompanyByUID(uid: string) {
-    return fromPromise(firestoreDB.collection("company").where(Filter.where('CompanyUID', '==', uid)).get())
+export function getCompanyByUID(uid: string): Observable<Company> {
+    const docPath = `company/${uid}`
+    return fromPromise(firestoreDB.doc(docPath).get()).pipe( map(d => {
+        return d.data() as Company
+    }))
 }
